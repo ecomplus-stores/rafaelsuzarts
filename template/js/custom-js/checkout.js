e09cf821a8318b253084e73c2bdcc42d415f4ca0
eduvlemes <edu.vlemes@gmail.com>
2021-05-25 18:35:11 -0300

    ajuste checkout.js

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -9,8 +9,8 @@ window.storefrontApp.router.afterEach(({ name, params }) => {
         const dataJSON = JSON.parse(decodeURIComponent(decodeURIComponent(data.replace(/\+/g,  " "))));
         console.log(dataJSON);
     
-        $('<img src="https://letsperformgo.go2cloud.org/aff_l?offer_id=135&adv_sub='+ dataJSON.number +'&amount='+ dataJSON.transactions[0].amount +'" width="1" height="1" />').appendTo('body');
-        $('<img src="https://mais2.co/aff/scripts/sale.php?AccountId=default1&TotalCost='+ dataJSON.transactions[0].amount +'&OrderID='+ dataJSON.number +'&ProductID=Ormizi&CampaignID=679ee28a&Commission=%20" width="1" height="1" >').appendTo('body');
-        $('<img src=https://pln.brtrk2.com/aff_goal?a=l&goal_name=sale&adv_id=2956&offer_id=7110&adv_sub='+ dataJSON.number +'&amount='+ dataJSON.transactions[0].amount +' width="1" height="1" />').appendTo('body');
+        $('<img src="https://letsperformgo.go2cloud.org/aff_l?offer_id=135&adv_sub='+ window.storefrontApp.router.currentRoute.params.id +'&amount='+ window.storefrontApp.amount.total +'" width="1" height="1" />').appendTo('body');
+        $('<img src="https://mais2.co/aff/scripts/sale.php?AccountId=default1&TotalCost='+ window.storefrontApp.amount.total +'&OrderID='+ window.storefrontApp.router.currentRoute.params.id +'&ProductID=Ormizi&CampaignID=679ee28a&Commission=%20" width="1" height="1" >').appendTo('body');
+        $('<img src=https://pln.brtrk2.com/aff_goal?a=l&goal_name=sale&adv_id=2956&offer_id=7110&adv_sub='+ window.storefrontApp.router.currentRoute.params.id +'&amount='+ window.storefrontApp.amount.total +' width="1" height="1" />').appendTo('body');
     }
 })
